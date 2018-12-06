a155efd0e8a9ac58cc1e039141efd4e51da71758
Jaxon Keeler <jaxon@artifylabs.com>
2018-12-06 15:59:49 -0700

    slots api has GET, POST, and PUT

--- api/models/index.js
+++ api/models/index.js
@@ -5,23 +5,23 @@ const Schema = mongoose.Schema,
   ObjectId = mongoose.Schema.Types.ObjectId;
 
 
-const slotSchema = new Schema ({
-  slot_time: String,
-  slot_date: String,
-  created_at: Date
-});
+// const slotSchema = new Schema ({
+//   slot_time: String,
+//   slot_date: String,
+//   created_at: Date
+// });
 
-const Slot = model('Slot', slotSchema);
+// const Slot = model('Slot', slotSchema);
 
-const appointmentSchema = new Schema({
-  id: ObjectId,
-  name: String,
-  phone: Number,
-  slots:{type: ObjectId, ref: 'Slot'},
-  created_at: Date
-});
+// const appointmentSchema = new Schema({
+//   id: ObjectId,
+//   name: String,
+//   phone: Number,
+//   slots:{type: ObjectId, ref: 'Slot'},
+//   created_at: Date
+// });
 
-const Appointment = model('Appointment', appointmentSchema);
+// const Appointment = model('Appointment', appointmentSchema);
 
 const slot2Schema = new Schema ({
   start: Date,
@@ -36,5 +36,6 @@ const slot2Schema = new Schema ({
 const Slot2 = model('Slot2', slot2Schema, 'slots2');
 
 module.exports = {
-  Appointment, Slot, Slot2
+  // Appointment, Slot, 
+  Slot2
 };
--- api/routes/api/index.js
+++ api/routes/api/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const appointmentController = require('../../controllers/appointments');
-const slotController = require('../../controllers/slot');
+// const appointmentController = require('../../controllers/appointments');
+// const slotController = require('../../controllers/slot');
 const slot2Controller = require('../../controllers/slot2');
 
-router.get('/appointments', appointmentController.all);
-router.get('/slots2', slot2Controller.all);
-router.post('/appointmentCreate', appointmentController.create);
+// router.get('/appointments', appointmentController.all);
+router.get('/slots', slot2Controller.all);
+router.post('/slots', slot2Controller.create);
+router.put('/slots/:slotId', slot2Controller.updateById);
+// router.post('/appointmentCreate', appointmentController.create);
 
 
 
