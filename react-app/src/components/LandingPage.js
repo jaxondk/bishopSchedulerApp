9a8d95d15a83cb8d67c60fbd0bd9d12a69d9ecda
ileana-heaton <ileana.heaton@byu.net>
2018-12-11 00:00:32 -0700

    awkward landing page

--- react-app/src/components/LandingPage.js
+++ react-app/src/components/LandingPage.js
@@ -2,19 +2,9 @@ import React, {Component} from 'react';
 import { Provider, Heading, Subhead } from 'rebass'
 // import { Heading } from 'rebass'
 import {
-  Hero, CallToAction, RaisedButton
+  Hero, CallToAction
 } from 'react-landing-page'
-
-const markdownStyles = {
-  // paragraph: {
-  //   fontSize: 14,
-  //   marginBottom: 10,
-  //   lineHeight: 21,
-  // },
-  // color: 'white'
-  
-}
-
+import Button from '@material-ui/core/Button';
 
 class LandingPage extends Component {
   render() {
@@ -22,21 +12,29 @@ class LandingPage extends Component {
       <Provider>
         <Hero
           color="black"
-          bg="#F0FFFF"
+          bg="background: #dbe8f9;
+          background: -moz-linear-gradient(top, #dbe8f9 41%, #b3cee5 64%, #87a0d3 98%, #829be0 100%);
+          background: -webkit-linear-gradient(top, #dbe8f9 41%,#b3cee5 64%,#87a0d3 98%,#829be0 100%);
+          background: linear-gradient(to bottom, #dbe8f9 41%,#b3cee5 64%,#87a0d3 98%,#829be0 100%);
+          filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#dbe8f9', endColorstr='#829be0',GradientType=0 );"
           // backgroundImage="https://source.unsplash.com/jxaj-UrzQbc/1600x900"
           // backgroundImage="https://amppob.com/wp-content/uploads/2018/06/iStock-916563360-810x405.jpg"
         >
-          <img src={require('./calendar.png')} width="20%"/>
-          <Heading style={markdownStyles}>Secretariat</Heading>
+          <img src={require('./calendar2.png')} width="25%"/>
+          <Heading>Secretariat</Heading>
           <br/>
-          <Subhead style={markdownStyles}>Manage a Bishop's schedule with ease</Subhead>
-          <p style={markdownStyles}>Keep schedules up to date with automated text reminders</p>
-          <CallToAction href="/getting-started" mt={3}>Get Started</CallToAction>
+          <Subhead>Manage a Bishop's schedule with ease</Subhead>
+          <p>Keep schedules up to date with automated text reminders</p>
+          {/* <CallToAction href="/getting-started" mt={3}>Get Started</CallToAction> */}
           {/* make CallToAction into a Rounded button */}
+          <Button variant="contained" size="large" color="primary" href="https://www.facebook.com/">
+          Get started
+        </Button>
         </Hero>
       </Provider>
     );
   }
 }
+
 
 export default LandingPage;
