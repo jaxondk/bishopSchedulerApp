5cbac6d83465a416c6c72316cdbc90f0c3d39380
ileana-heaton <ileana.heaton@byu.net>
2018-12-10 22:49:13 -0700

    messing with UI..

--- react-app/src/components/LandingPage.js
+++ react-app/src/components/LandingPage.js
@@ -2,8 +2,19 @@ import React, {Component} from 'react';
 import { Provider, Heading, Subhead } from 'rebass'
 // import { Heading } from 'rebass'
 import {
-  Hero, CallToAction, ScrollDownIndicator
+  Hero, CallToAction, RaisedButton
 } from 'react-landing-page'
+
+const markdownStyles = {
+  // paragraph: {
+  //   fontSize: 14,
+  //   marginBottom: 10,
+  //   lineHeight: 21,
+  // },
+  // color: 'white'
+  
+}
+
 
 class LandingPage extends Component {
   render() {
@@ -11,13 +22,17 @@ class LandingPage extends Component {
       <Provider>
         <Hero
           color="black"
-          bg="white"
-          backgroundImage="https://source.unsplash.com/jxaj-UrzQbc/1600x900"
+          bg="#F0FFFF"
+          // backgroundImage="https://source.unsplash.com/jxaj-UrzQbc/1600x900"
+          // backgroundImage="https://amppob.com/wp-content/uploads/2018/06/iStock-916563360-810x405.jpg"
         >
-          <Heading>Name of your app</Heading>
-          <Subhead>a couple more words</Subhead>
+          <img src={require('./calendar.png')} width="20%"/>
+          <Heading style={markdownStyles}>Secretariat</Heading>
+          <br/>
+          <Subhead style={markdownStyles}>Manage a Bishop's schedule with ease</Subhead>
+          <p style={markdownStyles}>Keep schedules up to date with automated text reminders</p>
           <CallToAction href="/getting-started" mt={3}>Get Started</CallToAction>
-          {/* <ScrollDownIndicator /> */}
+          {/* make CallToAction into a Rounded button */}
         </Hero>
       </Provider>
     );
